bf25ddb2ef64b761476e30c97cba73f20182f428
Pawel Trafimuk <pavel@viber.com>
2022-11-08 00:37:00 +0100

    added public flags

--- Sources/ViberBotSwiftSDK/SendMessageModels/SenderInfo.swift
+++ Sources/ViberBotSwiftSDK/SendMessageModels/SenderInfo.swift
@@ -10,4 +10,9 @@ import Foundation
 public struct SenderInfo: Codable {
     public let name: String
     public let avatar: URL?
+    
+    public init(name: String, avatar: URL? = nil) {
+        self.name = name
+        self.avatar = avatar
+    }
 }
