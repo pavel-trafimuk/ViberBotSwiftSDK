8a221c0d0220c3c5d20e05523904f9d7a4f5701f
Pawel Trafimuk <pavel@viber.com>
2023-05-15 10:41:44 +0200

    added logs

--- Sources/ViberBotSwiftSDK/ViberBotController.swift
+++ Sources/ViberBotSwiftSDK/ViberBotController.swift
@@ -45,8 +45,7 @@ public struct ViberBotController: RouteCollection {
             
             do {
                 logger.debug("12 check for event: \(req.viberBot.config.databaseLevel.rawValue)")
-                if req.viberBot.config.databaseLevel.contains(.callbackEvent),
-                let dbEvent = SavedCallbackEvent(event: event) {
+                if let dbEvent = SavedCallbackEvent(event: event) {
                     logger.debug("121 event created, trying to save")
                     do {
                         try await dbEvent.save(on: req.db)
