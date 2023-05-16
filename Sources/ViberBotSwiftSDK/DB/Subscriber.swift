a28e8e56c78c4f60c19ff45da9f0cdd16df07709
Pawel Trafimuk <pavel@viber.com>
2023-05-16 08:24:42 +0200

    tests

--- Sources/ViberBotSwiftSDK/DB/Subscriber.swift
+++ Sources/ViberBotSwiftSDK/DB/Subscriber.swift
@@ -38,30 +38,30 @@ public final class Subscriber: Model {
     @OptionalField(key: "api_version")
     public var apiVersion: Int?
 
-    @OptionalField(key: "primary_device_os")
-    public var primaryDeviceOS: String?
-
-    @OptionalField(key: "viber_version")
-    public var viberVersion: String?
-
-    @OptionalField(key: "device_type")
-    public var deviceType: String?
-
-    @OptionalField(key: "mcc")
-    public var mcc: Int?
-
-    @OptionalField(key: "mnc")
-    public var mnc: Int?
+//    @OptionalField(key: "primary_device_os")
+//    public var primaryDeviceOS: String?
+//
+//    @OptionalField(key: "viber_version")
+//    public var viberVersion: String?
+//
+//    @OptionalField(key: "device_type")
+//    public var deviceType: String?
+//
+//    @OptionalField(key: "mcc")
+//    public var mcc: Int?
+//
+//    @OptionalField(key: "mnc")
+//    public var mnc: Int?
 
     /// to map user with some external services
-    @OptionalField(key: "external_id")
-    public var externalId: String?
-
-    @OptionalField(key: "external_status")
-    public var externalStatus: String?
-
-    @OptionalField(key: "external_value")
-    public var externalValue: Data?
+//    @OptionalField(key: "external_id")
+//    public var externalId: String?
+//
+//    @OptionalField(key: "external_status")
+//    public var externalStatus: String?
+//
+//    @OptionalField(key: "external_value")
+//    public var externalValue: Data?
 
     /// Creates a new, empty Subscriber.
     public init() { }
