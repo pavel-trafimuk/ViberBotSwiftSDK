666a5b242985bdbc19de4b38aac5d94e7279e12b
Pawel Trafimuk <pavel@viber.com>
2023-05-16 15:44:13 +0200

    Reverted api version

--- Sources/ViberBotSwiftSDK/DB/Subscriber.swift
+++ Sources/ViberBotSwiftSDK/DB/Subscriber.swift
@@ -87,7 +87,7 @@ extension Subscriber {
         name = user.name ?? ""
         avatar = user.avatar
         country = user.country
-//        language = user.language
+        language = user.language
 //        apiVersion = user.apiVersion
     }
 }
